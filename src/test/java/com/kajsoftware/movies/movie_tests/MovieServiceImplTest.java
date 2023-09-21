06298124b05cde5eb45e785ff07bcc36e2a3398b
mizanur-16 <mizanur.dev16@gmail.com>
2023-09-21 16:50:31 +0600

    Unit Testing in progress

---
+++ src/test/java/com/kajsoftware/movies/movie_tests/MovieServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.kajsoftware.movies.movie_tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+public class MovieServiceImplTest {
+
+    @Test
+    void test() {
+
+    }
+}
